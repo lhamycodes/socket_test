5061c223f1c4f398a1a2c15eb23356a5075a7d24
Adedeji Rotibi <dayjeerow7@gmail.com>
2020-11-10 13:15:56 +0100

    I think I've tried

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
-      theme: new ThemeData(
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var mTextMessageController = new TextEditingController();
+  Socket socket;
+  var mTextMessageController = TextEditingController();
+
+  List _messages = [];
+  _log(String message) {
+    print(message);
+    _messages.add(message);
+    setState(() {});
+  }
 
   @override
   void initState() {
     super.initState();
+    socket = io('http://onlylogistics-core.herokuapp.com', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+    socket.connect();
+
+    socket.on('connect', (_) {
+      _log('Websocket connect');
+    });
+
+    socket.on("received-message", (data) => _log(data));
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Testing"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Testing Socket.io"),
       ),
-      body: new Center(
-        child: new Column(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[
+            Text("Websockets"),
+            SizedBox(height: 10),
+            ListView.builder(
+              itemBuilder: (cx, i) {
+                return Text(_messages[i]);
+              },
+              itemCount: _messages.length,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+            )
+          ],
         ),
       ),
     );
